e2602fcb77cc8569ada6799455713bc27a16af5e
Ying Chen <jummy1124@gmail.com>
2022-01-18 19:29:13 +0800

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bombcrypto auto click
+## Environment
+### Python 3.7.6 ; pyautogui 0.9.53
+## Demo
+https://youtu.be/mfQ_2wZcgE0
---
+++ main.py
@@ -0,0 +1,22 @@
+import time
+import pyautogui 
+
+# Screen Size => width=1920, height=1080
+
+while True:
+    time.sleep(3)
+    pyautogui.moveTo(950,910)
+    pyautogui.click()
+    time.sleep(2)
+    pyautogui.moveTo(950,890)
+    pyautogui.click()
+    time.sleep(2)
+    pyautogui.moveTo(850,440)
+    pyautogui.click()
+    time.sleep(2)
+    pyautogui.moveTo(1020,325)
+    pyautogui.click()
+    time.sleep(2)
+    pyautogui.moveTo(950,790)
+    pyautogui.click()
+    time.sleep(900)   #How long do you want to sleep
