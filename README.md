2f3eeace8b792c4fbc10a25ba3e1f4e2cfe1524c
Ying Chen <jummy1124@gmail.com>
2022-01-24 10:37:31 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Bombcrypto auto click
+## Update : 
+### For multi role to work (24 January 2022)
 ## Environment
 ### Python 3.7.6 ; pyautogui 0.9.53
 ## Demo
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import os
 import time
+from datetime import datetime
 import pyautogui 
 
 # Screen Size => width=1920, height=1080
 
 while True:
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time(work) =", current_time)
     time.sleep(3)
     pyautogui.moveTo(950,910)
     pyautogui.click()
@@ -11,7 +16,7 @@ while True:
     pyautogui.moveTo(950,890)
     pyautogui.click()
     time.sleep(2)
-    pyautogui.moveTo(850,440)
+    pyautogui.moveTo(850,390)
     pyautogui.click()
     time.sleep(2)
     pyautogui.moveTo(1020,325)
@@ -19,4 +24,25 @@ while True:
     time.sleep(2)
     pyautogui.moveTo(950,790)
     pyautogui.click()
-    time.sleep(900)   #How long do you want to sleep
+    time.sleep(180)   #How long do you want to work
+    
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time(wrest) =", current_time)
+    time.sleep(3)
+    pyautogui.moveTo(950,910)
+    pyautogui.click()
+    time.sleep(2)
+    pyautogui.moveTo(950,890)
+    pyautogui.click()
+    time.sleep(2)
+    pyautogui.moveTo(930,390)
+    pyautogui.click()
+    time.sleep(2)
+    pyautogui.moveTo(1020,325)
+    pyautogui.click()
+    time.sleep(2)
+    pyautogui.moveTo(950,790)
+    pyautogui.click()
+    time.sleep(720)   #How long do you want to rest
+    
